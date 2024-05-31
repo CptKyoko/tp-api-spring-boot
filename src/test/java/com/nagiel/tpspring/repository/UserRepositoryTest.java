fab7c771689d65ab5affaf3c27caded0f1671ec8
Guillaume Nagiel <guillaume.nagiel@gmail.com>
2024-05-31 14:54:15 +0200

    Modif TU

--- src/test/java/com/nagiel/tpspring/repository/UserRepositoryTest.java
+++ src/test/java/com/nagiel/tpspring/repository/UserRepositoryTest.java
@@ -5,16 +5,13 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.nagiel.tpspring.model.User;
 
-@RunWith(SpringRunner.class)
+//@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class UserRepositoryTest {
